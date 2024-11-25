86ee3c29260c01bf9c1e08f67d11f1e3cb4e3cb0
RudenkoBogdan <rudenko.ba03@gmail.com>
2024-11-25 23:36:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.testclient import TestClient
+import os
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+static_path = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/static", StaticFiles(directory=static_path), name="static")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/", response_class=HTMLResponse)
+async def resume(request: Request):
+    data = {
+        "name": "Bogdan Alenovich Rudenko",
+        "profession": "Data Science, Machine Learning",
+        "bio": "AI enthusiast, theoretical physicist, astronomer",
+        "skills": [
+            "Python for data analysis",
+            "NumPy, Pandas", 
+            "ML and DL",
+            "Sklearn, Pytorch", 
+            "Data Visualisation",
+            "Matplotlib, Seaborn, Plotly, Bokeh", 
+            "Math",
+            "Calculus, linear algebra, probability theory, mathematical statistics" 
+        ],
+        "contact": {
+            "email": "rudenko.ba03@gmail.com",
+            "phone": "+7 925 240 27 36",
+            "linkedin": "https://www.linkedin.com/in/bogdan-rudenko-3bb926323/",
+            "kaggle": "https://www.kaggle.com/rudenkobogdan",
+            "github": "https://github.com/RudenkoBogdan"
+        },
+    }
+    return templates.TemplateResponse("resume.html", {"request": request, "data": data})
